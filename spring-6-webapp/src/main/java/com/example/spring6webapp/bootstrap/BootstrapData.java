1f2868ae29ac3fea5c30bcc04ffdb05d83e850c2
Son Ta <tadinhson1999@gmail.com>
2023-03-16 17:09:33 +0700

    entity, h2, repository

---
+++ spring-6-webapp/src/main/java/com/example/spring6webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,46 @@
+package com.example.spring6webapp.bootstrap;
+
+import com.example.spring6webapp.domain.Author;
+import com.example.spring6webapp.domain.Book;
+import com.example.spring6webapp.repositories.AuthorRepository;
+import com.example.spring6webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<Author> authors = new ArrayList<>();
+        List<Book> books = new ArrayList<>();
+        IntStream.range(0, 5).forEachOrdered(n -> {
+            // author
+            Author newAuthor = new Author();
+            newAuthor.setLastName("Ta");
+            newAuthor.setFirstName("Son " + n);
+            authors.add(newAuthor);
+
+            // book
+            Book newBook = new Book();
+            newBook.setTitle("Quyen sach thu " + n);
+            newBook.setIsbn("12345" + n);
+            books.add(newBook);
+        });
+
+        authorRepository.saveAll(authors);
+        bookRepository.saveAll(books);
+    }
+}
