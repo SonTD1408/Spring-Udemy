45e216bdf6fec905fa8db03b02985464763b2a4c
Son Ta <tadinhson1999@gmail.com>
2023-03-17 17:31:42 +0700

    add service and thymeleaf

--- spring-6-webapp/src/main/java/com/example/spring6webapp/bootstrap/BootstrapData.java
+++ spring-6-webapp/src/main/java/com/example/spring6webapp/bootstrap/BootstrapData.java
@@ -2,13 +2,16 @@ package com.example.spring6webapp.bootstrap;
 
 import com.example.spring6webapp.domain.Author;
 import com.example.spring6webapp.domain.Book;
+import com.example.spring6webapp.domain.Publisher;
 import com.example.spring6webapp.repositories.AuthorRepository;
 import com.example.spring6webapp.repositories.BookRepository;
+import com.example.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.IntStream;
 
 @Component
@@ -16,16 +19,21 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository,
+                         BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         List<Author> authors = new ArrayList<>();
         List<Book> books = new ArrayList<>();
+        List<Publisher> publishers = new ArrayList<>();
 
         IntStream.range(0, 5).forEachOrdered(n -> {
             // author
@@ -39,12 +47,23 @@ public class BootstrapData implements CommandLineRunner {
             newBook.setTitle("Quyen sach thu " + n);
             newBook.setIsbn("12345" + n);
             books.add(bookRepository.save(newBook));
+
+            // publisher
+            Publisher newPublisher = new Publisher();
+            newPublisher.setPublisherName("Son " + n);
+            newPublisher.setAddress("Dia Chi" + n);
+            publishers.add(publisherRepository.save(newPublisher));
         });
 
+        // set book for author
         IntStream.range(0, authors.size()).forEachOrdered(i -> {
-            authors.get(i).getBooks().add(books.get(i));
+            authors.get(i).setBooks(Set.of(books.get(i)));
+            publishers.get(i).setBooks(Set.of(books.get(i)));
+            books.get(i).setAuthors(Set.of(authors.get(i)));
+            books.get(i).setPublisher(publishers.get(i));
         });
         authorRepository.saveAll(authors);
-        System.out.println(authorRepository.count());
+        bookRepository.saveAll(books);
+        publisherRepository.saveAll(publishers);
     }
 }
